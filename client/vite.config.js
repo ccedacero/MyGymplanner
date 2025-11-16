d355ad9e29185373735eb75e1a5b5df9d93f4ea1
Cristian Cedacero <devtzi@Cristians-MBP.lan>
2025-11-16 17:39:52 -0500

    Fix Vite preview port for Railway

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,6 +13,10 @@ export default defineConfig({
       }
     }
   },
+  preview: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    host: '0.0.0.0'
+  },
   build: {
     outDir: 'dist',
     sourcemap: false
